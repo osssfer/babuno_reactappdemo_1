757dfc47a05dce7f85d899eedc2d929eda8e4e1a
osssfer <79018846+osssfer@users.noreply.github.com>
2021-05-03 19:14:08 -0400

    'cambios_bootstrap'

--- src/App.js
+++ src/App.js
@@ -13,14 +13,7 @@ function App() {
           WENAAAA LO CTM !!! 
         </p>
         <Button>tesst </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
